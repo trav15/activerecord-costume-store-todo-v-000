b0ff845303d06e1c6813b89c0820f55afa59e119
Travis T <travistokuyama@gmail.com>
2018-11-07 16:40:21 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,6 +5,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
       t.timestamps
     end
   end
