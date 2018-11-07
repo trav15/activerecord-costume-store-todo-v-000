56964bc789b2063a24053b42e178f84d476664f6
Travis T <travistokuyama@gmail.com>
2018-11-07 16:39:17 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,28 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
+    t.float "price"
+    t.string "image_url"
+    t.string "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
+    t.string "location"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
