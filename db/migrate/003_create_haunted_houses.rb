98739df318c570c2a6aeb8adee8b87e88bd773fe
Travis T <travistokuyama@gmail.com>
2018-11-07 16:38:50 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,6 +2,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
+      t.string :location
       t.timestamps
     end
   end
