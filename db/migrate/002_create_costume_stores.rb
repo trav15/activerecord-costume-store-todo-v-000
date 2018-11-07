01cb43f27f48b345852e17eeb37ccd9a37c9bf06
Travis T <travistokuyama@gmail.com>
2018-11-07 16:33:10 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
+      t.string :location
+      t.timestamps
     end
   end
 end
